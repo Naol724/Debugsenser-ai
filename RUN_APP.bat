696a19db9b127e8b934976619af8d46cbb05c4f2
Naol Gonfa <naolgonfa449@gmail.com>
2026-05-03 02:29:24 +0300

    unneeded file is deleted from project

--- RUN_APP.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo Starting DebugSense AI Application
-echo ========================================
-echo.
-echo Make sure you are in the debugsense-ai folder!
-echo.
-cd /d "%~dp0"
-npm run dev
